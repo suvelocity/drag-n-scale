2744f801a1b1586153d00a6835652bc0bf7861dd
listguy <nitzanjl@gmail.com>
2020-10-04 15:10:51 +0300

    added itay tests + package-lock

--- drag.test.js
+++ drag.test.js
@@ -1,18 +1,19 @@
 const puppeteer = require("puppeteer");
 describe("dragging box", () => {
   test("dragging", async () => {
-    const browser = await puppeteer.launch(
+    const browser = await puppeteer
+      .launch
       // {
       //   headless: false,
       //   slowMo: 100,
       // }
-    );
-    const url = `file://${__dirname}/../index.html`
+      ();
+    const url = `file://${__dirname}/../index.html`;
 
     const page = await browser.newPage();
     await page.goto(url);
 
-    const header = await page.$('#header');
+    const header = await page.$("#header");
 
     const { top, left } = await page.evaluate((header) => {
       let { top, left } = header.getBoundingClientRect();
@@ -23,64 +24,111 @@ describe("dragging box", () => {
 
     await page.mouse.move(left, top);
     await page.mouse.down();
-    await page.mouse.move(2 * left, 2 * top);
+    await page.mouse.move(left + 20, top + 20);
     await page.mouse.up();
 
     const { newTop, newLeft } = await page.evaluate((header) => {
       const { top, left } = header.getBoundingClientRect();
-      return { newTop: top + header.clientHeight / 2, newLeft: left + header.clientWidth / 2 };
+      return {
+        newTop: top + header.clientHeight / 2,
+        newLeft: left + header.clientWidth / 2,
+      };
     }, header);
 
-    // expect(newLeft).toBe(2 * left)
-    // expect(newTop).toBe(2 * top)
-    expect(true).toBe(true)
+    expect(newLeft).toBe(left + 20);
+    expect(newTop).toBe(top + 20);
+
     await browser.close();
   });
   test("can't go past the borders", async () => {
-    const browser = await puppeteer.launch(
+    const browser = await puppeteer
+      .launch
       // {
       //   headless: false,
       //   slowMo: 100,
       // }
-    );
-    const url = `file://${__dirname}/../index.html`
+      ();
+    const url = `file://${__dirname}/../index.html`;
     const page = await browser.newPage();
     await page.goto(url);
 
-    // const playground = await page.$('#playground');
-    // const { playTop, playLeft, playBottom, playRight } = await page.evaluate((playground) => {
-    //   let { top, left, right, bottom } = box.getBoundingClientRect();
-    //   return { playTop: top, playLeft: left, playtBottom: bottom, playRight: right };
-    // }, playground);
+    const playground = await page.$("#playground");
+    const { playTop, playLeft, playBottom, playRight } = await page.evaluate(
+      (playground) => {
+        let { top, left, right, bottom } = playground.getBoundingClientRect();
+        return {
+          playTop: top,
+          playLeft: left,
+          playBottom: bottom,
+          playRight: right,
+        };
+      },
+      playground
+    );
 
-    // const box = await page.$('#main');
-    // const { boxWidth, boxHeight } = await page.evaluate((box) => {
-    //   let { width, height } = box.getBoundingClientRect();
-    //   return { boxWidth: width, boxHeight: height };
-    // }, box);
+    const box = await page.$("#main");
+    const { boxWidth, boxHeight } = await page.evaluate((box) => {
+      let { width, height } = box.getBoundingClientRect();
+      return { boxWidth: width, boxHeight: height };
+    }, box);
 
-    // const header = await page.$('#header');
+    const header = await page.$("#header");
 
-    // const { y, x } = await page.evaluate((header) => {
-    //   let { top, left, width, height } = header.getBoundingClientRect();
-    //   return { y: top + height / 2, x: left + width / 2, headerWidth: width, headerHeight: height };
-    // }, header);
+    const { y, x, headerWidth, headerHeight } = await page.evaluate(
+      (header) => {
+        let { top, left, width, height } = header.getBoundingClientRect();
+        return {
+          y: top + height / 2,
+          x: left + width / 2,
+          headerWidth: width,
+          headerHeight: height,
+        };
+      },
+      header
+    );
 
-    // await page.mouse.move(x, y);
-    // await page.mouse.down();
-    // await page.mouse.move(playBottom, playRight);
-    // await page.mouse.up();
+    const boxOnBorderRight = playRight - boxWidth + headerWidth / 2;
+    const boxOnBorderBottom = playBottom - boxHeight + headerHeight / 2;
+    const boxOnBorderLeft = playLeft + headerWidth / 2;
+    const boxOnBorderTop = playTop + headerHeight / 2;
 
-    // const { newTop, newLeft } = await page.evaluate((box) => {
-    //   const { top, left } = box.getBoundingClientRect();
-    //   return { newTop: top, newLeft: left };
-    // }, box);
+    await page.mouse.move(x, y);
+    await page.mouse.down();
+    await page.mouse.move(boxOnBorderRight, boxOnBorderBottom);
+    await page.mouse.move(boxOnBorderRight + 10, boxOnBorderBottom + 10);
+    await page.mouse.up();
 
-    // expect(newLeft).toBe(playRight - boxWidth);
-    // expect(newTop).toBe(playBottom - boxHeight);
-    expect(true).toBe(true)
+    const {
+      onBottomRightCornerTop,
+      onBottomRightCornerLeft,
+    } = await page.evaluate((box) => {
+      const { top, left } = box.getBoundingClientRect();
+      return { onBottomRightCornerTop: top, onBottomRightCornerLeft: left };
+    }, box);
+
+    expect(onBottomRightCornerLeft).toBe(playRight - boxWidth);
+    expect(onBottomRightCornerTop).toBe(playBottom - boxHeight);
+
+    await page.mouse.move(
+      onBottomRightCornerLeft + headerWidth / 2,
+      onBottomRightCornerTop + headerHeight / 2
+    );
+    await page.mouse.down();
+    await page.mouse.move(boxOnBorderLeft, boxOnBorderTop);
+    await page.mouse.move(boxOnBorderLeft - 10, boxOnBorderTop - 10);
+    await page.mouse.up();
+
+    const { onTopLeftCornerTop, onTopLeftCornerLeft } = await page.evaluate(
+      (box) => {
+        const { top, left } = box.getBoundingClientRect();
+        return { onTopLeftCornerTop: top, onTopLeftCornerLeft: left };
+      },
+      box
+    );
+
+    expect(onTopLeftCornerLeft).toBe(playLeft);
+    expect(onTopLeftCornerTop).toBe(playTop);
 
     await browser.close();
-  })
-
-})
+  });
+});
